3b69c5d257a30952264b8a9261801418485eaa44
Harsh Maurya <hm42638hh@gmail.com>
2026-07-12 20:37:05 +0530

    Added email service with custom exception

--- backend/src/main/java/com/stockup/backend/infrastructure/notification/email/provider/BrevoEmailProvider.java
+++ backend/src/main/java/com/stockup/backend/infrastructure/notification/email/provider/BrevoEmailProvider.java
@@ -1,9 +1,12 @@
 package com.stockup.backend.infrastructure.notification.email.provider;
 
 import com.stockup.backend.common.config.properties.EmailProperties;
+import com.stockup.backend.infrastructure.notification.email.exception.EmailDeliveryException;
 import com.stockup.backend.infrastructure.notification.email.provider.brevo.dto.RecipientDto;
 import com.stockup.backend.infrastructure.notification.email.provider.brevo.dto.SendEmailRequest;
 import com.stockup.backend.infrastructure.notification.email.provider.brevo.dto.SenderDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -20,6 +23,8 @@ public class BrevoEmailProvider implements EmailProvider {
 
     private final RestClient restClient;
     private final EmailProperties emailProperties;
+
+    private static final Logger log = LoggerFactory.getLogger(BrevoEmailProvider.class);
 
     public BrevoEmailProvider(
             RestClient restClient,
@@ -42,13 +47,28 @@ public class BrevoEmailProvider implements EmailProvider {
                 "<pre>" + body + "</pre>"
         );
 
-        var response = restClient.post()
-                .uri(emailProperties.getBaseUrl() + "/v3/smtp/email")
-                .header("api-key", emailProperties.getApiKey())
-                .header("accept", "application/json")
-                .header("content-type", "application/json")
-                .body(request)
-                .retrieve()
-                .toEntity(String.class);
+        try {
+            restClient.post()
+                    .uri(emailProperties.getBaseUrl() + "/v3/smtp/email")
+                    .header("api-key", emailProperties.getApiKey())
+                    .header("accept", "application/json")
+                    .header("content-type", "application/json")
+                    .body(request)
+                    .retrieve()
+                    .toEntity(String.class);
+
+            log.info("Email sent successfully to {}", to);
+
+        } catch (Exception ex) {
+
+            log.error("Failed to send email to {}", to, ex);
+
+            throw new EmailDeliveryException(
+                    "Failed to deliver email using Brevo.",
+                    ex
+            );
+        }
+
+
     }
 }
